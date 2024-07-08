a17eb9a008834268119cf1122719f43ab02e3795
sint18 <htoohtoo.slh@gmail.com>
2024-07-08 02:22:44 +0100

    nav added

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let text: string
+    export let href: string
+</script>
+
+<li>
+    <a href={href}
+       class="underline underline-offset-8 text-xl sm:text-2xl font-semibold">
+        {text}
+    </a>
+</li>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import NavItem from "./NavItem.svelte";
+
+    interface Links {
+        text: string
+        href: string
+    }
+
+    const links: Links[] = [
+        {text: "Sint.", href: "/"},
+        {text: "Projects", href: "/projects"},
+        {text: "About", href: "/about"},
+    ]
+
+</script>
+
+<nav class="p-10 container mx-auto">
+    <ul class="flex justify-end space-x-10">
+        {#each links as link }
+            <NavItem text={link.text} href={link.href}></NavItem>
+        {/each}
+    </ul>
+</nav>

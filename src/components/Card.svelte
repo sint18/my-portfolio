8bb453cea847268dfd68243fe1aaab9db21310c4
sint18 <htoohtoo.slh@gmail.com>
2024-07-08 14:12:34 +0100

    project showcase cards added

---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let title: string
+    export let href: string
+</script>
+<div class="card card-hover">
+    <header>
+        <img class="w-full" src="https://placehold.co/600x400" alt="Cover image for {title}">
+    </header>
+    <section class="p-4 space-y-4">
+        <h2 class="text-2xl">{title}</h2>
+        <article>
+            <p>
+                Project Description
+            </p>
+        </article>
+    </section>
+    <footer class="card-footer">
+        <div class="flex space-x-4">
+
+        </div>
+    </footer>
+</div>

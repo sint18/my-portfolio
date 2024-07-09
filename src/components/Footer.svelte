b7488be08c0461ed4f7071867bfd4fbfe85ae39e
sint18 <htoohtoo.slh@gmail.com>
2024-07-09 01:31:38 +0100

    added icons and added icons to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+    import GitHubLogo from 'virtual:icons/mdi/github'
+    import LinkedInLogo from 'virtual:icons/mdi/linkedin'
+    import EmailIcon from 'virtual:icons/mdi/alternate-email'
+
     interface contactInfo {
         icon: string
         text: string
@@ -12,15 +16,16 @@
 </script>
 <footer>
     <div class="container mx-auto p-10 space-y-5">
-        <a href="/about" class="text-xl text-center">About</a>
-        <ul class="flex justify-between space-x-10">
-            {#each contactInfoList as contactInfo }
-                <li>
-                    <span>{contactInfo.icon}</span>
-                    <span class="flex-auto">{contactInfo.text}</span>
-                </li>
-            {/each}
-        </ul>
-        <p class="font-semibold text-end">Coded with ❤️ by Sint.</p>
+        <div class="flex space-x-5 justify-between">
+            <div class="flex space-x-5">
+                <a href="https://github.com/sint18" target="_blank" rel="noopener noreferrer" class="hover:text-primary-700 text-2xl">
+                    <GitHubLogo></GitHubLogo>
+                </a>
+                <a href="https://www.linkedin.com/in/sintlwinhtoo/" target="_blank" rel="noopener noreferrer" class="hover:text-primary-700 text-2xl">
+                    <LinkedInLogo></LinkedInLogo>
+                </a>
+            </div>
+            <p class="font-semibold">Coded with ❤️ by Sint.</p>
+        </div>
     </div>
 </footer>

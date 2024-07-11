2f25a94a373533225e9351d0da87e69119058c4f
sint18 <htoohtoo.slh@gmail.com>
2024-07-11 12:47:25 +0100

    changed responsiveness

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,10 +3,12 @@
     export let href: string
     export let description: string
     export let techStack: string[]
+    export let imageUrl: string
+
 </script>
 <a class="card card-hover overflow-hidden" href={href}>
     <header>
-        <img class="w-full" src="https://placehold.co/600x400" alt="Cover image for {title}">
+        <img class="w-full" src="{imageUrl}" alt="Cover image for {title}">
     </header>
     <section class="p-4 space-y-4">
         <h2 class="text-2xl">{title}</h2>
@@ -17,9 +19,9 @@
         </article>
     </section>
     <footer class="card-footer">
-        <div class="flex space-x-4">
+        <div class="flex flex-wrap gap-4">
             {#each techStack as lang}
-                <span class="chip variant-filled ">{lang}</span>
+                <span class="chip variant-filled">{lang}</span>
             {/each}
         </div>
     </footer>
